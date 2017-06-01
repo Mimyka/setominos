ac5e46f16d3c53d4fb20cfdfeb64c0a2226e6dce
Unknown <REMIDG>
2017-06-01 21:27:05 +0200

    Modal done

--- js/script.js
+++ js/script.js
@@ -31,18 +31,18 @@ var form2 = document.querySelector('#modal-buy .form_part2');
 
 choice.querySelector('.btn').onclick = function() {
   choice.style.display = "none";
-  form1.style.display = "block";
+  form1.style.display = "flex";
   form2.style.display = "none";
 }
 
 form1.querySelector('.btn').onclick = function() {
   choice.style.display = "none";
   form1.style.display = "none";
-  form2.style.display = "block";
+  form2.style.display = "flex";
 }
 
 function resetDisplay() {
-  choice.style.display = "block";
+  choice.style.display = "flex";
   form1.style.display = "none";
   form2.style.display = "none";
 }
