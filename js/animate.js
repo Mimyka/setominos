46a1fe5a110794bee5b23331499b98e4527416d9
Mimyka <leprunenec.jeremy@gmail.com>
2017-06-18 15:53:15 +0200

    Convert all animation into scrollObject

--- js/animate.js
+++ js/animate.js
@@ -18,26 +18,34 @@ function styleList(object) {
 	}
 };
 
-function linkObject(obj) {
+function scrollObject(obj) {
 	var _ = this;
 	_.target = obj.target;
-	_.from = obj.from;
-	_.to = obj.to;
+	_.from = obj.from || obj.target.parentNode;
+	_.to = obj.to || obj.target.parentNode;
 	_.start = obj.animation.start || {'x':0,'y':0};
 	_.end = obj.animation.end || {'x':0,'y':0};
+	_.start.unit = (obj.animation.start.unit && obj.animation.start.unit == "%")?"%":"px";
+	_.end.unit = (obj.animation.end.unit && obj.animation.end.unit == "%")?"%":"px";
+	if (_.start.unit == "%") {
+		_.start = {x: (obj.animation.start.x/100)*Number(_.from.offsetLeft), y: (obj.animation.start.y/100)*Number(_.from.offsetLeft)};
+	}
+	if (_.end.unit == "%") {
+		_.end = {x: (obj.animation.end.x/100)*Number(_.to.offsetLeft), y: (obj.animation.end.y/100)*Number(_.to.offsetLeft)};
+	}
 	_.onstart = obj.animation.onstart || function(){};
 	_.onprogress = obj.animation.onprogress || function(){};
 	_.onend = obj.animation.onend || function(){};
-
-	// optimise a/b declaration to prepare simple scroll object
-	_.a = {x: (Number(obj.from.offsetLeft) + _.start.x), y: (Number(obj.from.offsetTop) + _.start.y)};
-	_.b = {x: (Number(obj.to.offsetLeft) + _.end.x), y: (Number(obj.to.offsetTop) + _.end.y)};
+	_.a = {x: (Number(_.from.offsetLeft) + _.start.x), y: (Number(_.from.offsetTop) + _.start.y)};
+	_.b = {x: (Number(_.to.offsetLeft) + _.end.x), y: (Number(_.to.offsetTop) + _.end.y)};
 	_.ab = {
 		x: _.b.x - _.a.x,
 		y: _.b.y - _.a.y
 	};
 	_.translate = obj.translate || "-50%, -50%";
-	transformValue("rotate", obj.animation.rotate);
+	transformValue("rotateZ", obj.animation.rotateZ);
+	transformValue("rotateX", obj.animation.rotateX);
+	transformValue("rotateY", obj.animation.rotateY);
 	_.begin = obj.animation.begin || 0;
 
 	function transformValue(trgt, eql) {
@@ -70,15 +78,28 @@ function linkObject(obj) {
 		}
 	}
 	_.mv = function() {
-		_.target.style.left = ((_.ab.x*_.progress)+(_.start.x))+'px';
-		_.target.style.top = ((_.ab.y*_.progress)+(_.start.y))+'px';
-		_.target.style.transform = "translate("+_.translate+") rotate(" + ((_.rotate.start*(1-_.progress)) + (_.rotate.end*_.progress)) + "deg)";
+		_.target.style.left = ((_.ab.x*_.progress)+(_.start.x))+"px";
+		_.target.style.top = ((_.ab.y*_.progress)+(_.start.y))+"px";
+		_.target.style.transform = "translate("+_.translate+") rotateZ(" + ((_.rotateZ.start*(1-_.progress)) + (_.rotateZ.end*_.progress)) + "deg) rotateX(" + ((_.rotateX.start*(1-_.progress)) + (_.rotateX.end*_.progress)) + "deg) rotateY(" + ((_.rotateY.start*(1-_.progress)) + (_.rotateY.end*_.progress)) + "deg)";
 	}
 
 }
 
-var toAnimate = [
-	new linkObject({
+
+var desktopAnimation = [
+	new scrollObject({
+		target: document.querySelector('.seto_babystep'),
+		animation: {
+			begin: 400,
+			start: {"x":-140,"y":-90,"unit":"%"},
+			end: {"x":-30,"y":-27},
+			rotateZ: {
+				start: 30,
+				end: -50
+			}
+		}
+	}),
+	new scrollObject({
 		target: document.querySelector('.minos_babystep'),
 		from: document.querySelector('#babystep .piece'),
 		to: document.querySelector('#move .piece'),
@@ -86,13 +107,13 @@ var toAnimate = [
 			begin: 300,
 			start: {"x":39,"y":31},
 			end: {"x":-123,"y":85},
-			rotate: {
+			rotateZ: {
 				start: 10,
 				end: 0
 			}
 		}
 	}),
-	new linkObject({
+	new scrollObject({
 		target: document.querySelector('.minos_move'),
 		from: document.querySelector('#move .piece'),
 		to: document.querySelector('#mecanic .piece'),
@@ -113,7 +134,43 @@ var toAnimate = [
 			end: {"x":249,"y":-144}
 		}
 	}),
-	new linkObject({
+	new scrollObject({
+		target: document.querySelector('.seto_move.btmL'),
+		animation: {
+			begin: 200,
+			start: {"x":140,"y":-60,"unit":"%"},
+			end: {"x":48,"y":-14},
+			rotateZ: 240
+		}
+	}),
+	new scrollObject({
+		target: document.querySelector('.seto_mecanic.topL'),
+		animation: {
+			begin: 300,
+			start: {"x":-140,"y":-80,"unit":"%"},
+			end: {"x":-78,"y":-45},
+			rotateZ: -60
+		}
+	}),
+	new scrollObject({
+		target: document.querySelector('.seto_mecanic.btmL'),
+		animation: {
+			begin: 300,
+			start: {"x":-78,"y":45},
+			end: {"x":-140,"y":80,"unit":"%"},
+			rotateZ: 240
+		}
+	}),
+	new scrollObject({
+		target: document.querySelector('.seto_strategy2.btmR'),
+		animation: {
+			begin: 300,
+			start: {"x":295,"y":171},
+			end: {"x":140,"y":80,"unit":"%"},
+			rotateZ: 120
+		}
+	}),
+	new scrollObject({
 		target: document.querySelector('.seto_mecanic3.topL'),
 		from: document.querySelector('#mecanic .piece'),
 		to: document.querySelector('#strategy .piece'),
@@ -121,7 +178,32 @@ var toAnimate = [
 			begin: 200,
 			start: {"x":171,"y":99},
 			end: {"x":-46,"y":-27},
-			rotate: -60
+			rotateZ: -60
+		}
+	})
+];
+
+var mobileAnimation = [
+	new scrollObject({
+		target: document.querySelector('.seto_move.btmL'),
+		animation: {
+			begin: 200,
+			start: {"x":100,"y":-60},
+			end: {"x":48,"y":-14},
+			rotateZ: 240,
+			rotateX:{
+				start: 90,
+				end: 0
+			}
+		}
+	}),
+	new scrollObject({
+		target: document.querySelector('.seto_strategy.topL'),
+		animation: {
+			begin: 200,
+			start: {"x":-90,"y":-50},
+			end: {"x":-46,"y":-27},
+			rotateZ: -60
 		}
 	})
 ];
@@ -130,6 +212,8 @@ function animate(x){
 	posScroll = (document.querySelector('body').scrollTop != 0) ? document.querySelector('body').scrollTop : document.querySelector('html').scrollTop;
 	var x = ((posScroll / totalH) * 100).toFixed(2);
 	if (window.matchMedia("(min-width: 750px)").matches) {
+
+		// improve scrollObject to replace them when media match
 		styleList({
 			target: document.querySelector('.seto_strategy.topL'),
 			css: [['transform', 'translate(-50%, -50%) rotate(-60deg)'],['left', -46+'px']]
@@ -139,96 +223,18 @@ function animate(x){
 			css: [['transform', 'translate(-50%, -50%) rotate(240deg)'],['left', 48+'px']]
 		});
 
-		if (x <= 22.5) {
-			styleList({
-				target: document.querySelector('.seto_babystep'),
-				css: [['transform', 'translate(-50%, -50%) rotate('+(120-(x*6))+'deg)'],['left', -680+(x*27)+'px'],['top', -600+(x*24)+'px']]
-			});
-		}else{
-			styleList({
-				target: document.querySelector('.seto_babystep'),
-				css: [['transform', 'translate(-50%, -50%) rotate(-50deg)'],['left', -30+'px'],['top', -27+'px']]
-			});
-		}
-
-		if (x <= 42.5) {
-			styleList({
-				target: document.querySelector('.seto_move.btmL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(240deg)'],['left', 2000-(x*46.25)+'px'],['top', -1070+(x*25)+'px']]
-			});
-		}else{
-			styleList({
-				target: document.querySelector('.seto_move.btmL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(240deg)'],['left', 48+'px'],['top', -14+'px']]
-			});
-		}
-
-		if (x <= 58) {
-			styleList({
-				target: document.querySelector('.seto_mecanic.topL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(-60deg)'],['left', -1600+(x*26.4)+'px'],['top', -840+(x*13.7)+'px']]
-			});
-		}else{
-			styleList({
-				target: document.querySelector('.seto_mecanic.topL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(-60deg)'],['left', -78+'px'],['top', -45+'px']]
-			});
-		}
-
-		if (x >= 60) {
-			styleList({
-				target: document.querySelector('.seto_mecanic.btmL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(240deg)'],['left', 1600-(x*28)+'px'],['top', -840+(x*14.7)+'px']]
-			});
-		}else{
-			styleList({
-				target: document.querySelector('.seto_mecanic.btmL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(240deg)'],['left', -78+'px'],['top', 45+'px']]
-			});
-		}
-
-		if (x >= 75) {
-			styleList({
-				target: document.querySelector('.seto_strategy2.btmR'),
-				css: [['transform', 'translate(-50%, -50%) rotate(120deg)'],['left', -1200+(x*20)+'px'],['top', -730+(x*12)+'px']]
-			});
-		}else{
-			styleList({
-				target: document.querySelector('.seto_strategy2.btmR'),
-				css: [['transform', 'translate(-50%, -50%) rotate(120deg)'],['left', 295+'px'],['top', 171+'px']]
-			});
-		}
-
-		for (var i = 0; i < toAnimate.length; i++) {
-			toAnimate[i].animate(posScroll);
+		for (var i = 0; i < desktopAnimation.length; i++) {
+			desktopAnimation[i].animate(posScroll);
 		}
 
 	} else {
-		if (x <= 52.5) {
-			styleList({
-				target: document.querySelector('.seto_move.btmL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(240deg) rotateX('+(52.5-x)*5+'deg)'],['left', 258-(x*4)+'px']]
-			});
-		}else{
-			styleList({
-				target: document.querySelector('.seto_move.btmL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(240deg)'],['left', 48+'px']]
-			});
-		}
 		styleList({
 			target: document.querySelector('#mecanic .piece'),
 			css: [['transform', 'rotate('+(x-50)*5+'deg)']]
 		});
-		if (x <= 81) {
-			styleList({
-				target: document.querySelector('.seto_strategy.topL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(-'+x*5.2+'deg)'],['left', (Math.cos(x * 0.3029) * 80)-120+'px']]
-			});
-		} else{
-			styleList({
-				target: document.querySelector('.seto_strategy.topL'),
-				css: [['transform', 'translate(-50%, -50%) rotate(-60deg)'],['left', -46+'px'],['top', -27+'px']]
-			});
+
+		for (var i = 0; i < mobileAnimation.length; i++) {
+			mobileAnimation[i].animate(posScroll);
 		}
 	}
 }
