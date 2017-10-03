f0b86d7a49a2d04d40d97d2243361e3a42bfad06
Mimyka <jeremylp96@gmail.com>
2017-10-04 00:57:11 +0200

    Add mail function

--- form.php
+++ form.php
@@ -1,6 +1,8 @@
 <?php
-
   header("Content-Type: text/plain");
+
+  include './PHPMailer/class.phpmailer.php';
+  include './PHPMailer/class.smtp.php';
 
   $firstName = (isset($_POST['first_name'])) ? $_POST['first_name'] : "" ;
   $lastName = (isset($_POST['last_name'])) ? $_POST['last_name'] : "" ;
@@ -25,9 +27,6 @@
     preg_match("#^0[0-9]{9}$#", $phone)
   );
 
-  include './PHPMailer/class.phpmailer.php';
-  include './PHPMailer/class.smtp.php';
-
   foreach ($toverif as $value) {
     if (!$value) {
       $match = false;
@@ -39,22 +38,32 @@
   if ($match) {
       $message =
       "
-      Sample text $first_name
+      Bonjour,<br><br>
+      Vous avez reçu une commande de $amount boîte".(($amount==1)? "":"s")." de la part de <b>$firstName $lastName</b>.<br><br>
+      Voici ses coordonnées :<br>
+      <u>Nom</u> : $firstName<br>
+      <u>Prénom</u> : $lastName<br>
+      <u>Email</u> : $email<br>
+      <u>Adresse</u> : $adress<br>
+      <u>Code postal</u> : $postalCode<br>
+      <u>Ville</u> : $city<br>
+      <u>Téléphone</u> : $phone<br>
       ";
       $mail = new PHPMailer();
-      $mail->IsSMTP();
-      $mail->SMTPAuth = true;
-      $mail->Host = "host";
-      $mail->Port = 587;
-      $mail->Username = "mail";
-      $mail->Password = "pass";
-      $mail->SetFrom('from');
-      $mail->Subject = "Subject";
-      $mail->MsgHTML($message);
-      $mail->AddAddress('leprunenec.jeremy@gmail.com', 'Jeremy Le Prunenec');
-      if($mail->Send()) {
+      try {
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true;
+        $mail->Host = "smtp.gmail.com";
+        $mail->Port = 587;
+        $mail->Username = "virtual.assistant.setominos@gmail.com";
+        $mail->Password = "";
+        $mail->SetFrom('virtual.assistant.setominos@gmail.com','from');
+        $mail->Subject = "[$phone] Vous avez recu une commande de $amount boite".(($amount==1)? "":"s");
+        $mail->MsgHTML($message);
+        $mail->AddAddress('leprunenec.jeremy@gmail.com', 'Jeremy Le Prunenec');
+        $mail->Send();
         echo "true";
-      } else {
+      } catch (Exception $e) {
         echo $mail->ErrorInfo;
       }
   }else{
